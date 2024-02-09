d3c41fb32c7b04ae15466ab32e7407090b72da80
Julio Cesar <contato.jcsalerno@gmail.com>
2024-02-08 21:06:40 -0300

    feat: create rescuing everyone at parties

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -1,5 +1,47 @@
 const PartyModel = require("../models/Party");
 
-const partyController = {};
+const checkPartyBudget = (budget, services) => {
+  const priceSum = services.reduce((sum, service) => sum + service.price, 0);
+
+  if (priceSum > budget) {
+    return false;
+  }
+
+  return true;
+};
+
+const partyController = {
+  create: async (req, res) => {
+    try {
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+        return;
+      }
+
+      const response = await PartyModel.create(party);
+
+      res.status(201).json({ response, msg: "Festa criada com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getAll: async (req, res) => {
+    try {
+      const parties = await PartyModel.find();
+      res.json(parties);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
 module.exports = partyController;
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 const partyController = require("../controllers/partyController");
 
+router.route("/parties").post((req, res) => partyController.create(req, res));
+
+router.route("/parties").get((req, res) => partyController.getAll(req, res));
+
 module.exports = router;
