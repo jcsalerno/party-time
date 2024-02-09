b67a818b7de2a2f1af9d1ecff56547f2227b9eee
Julio Cesar <contato.jcsalerno@gmail.com>
2024-02-08 22:44:07 -0300

    feat: finished the backend

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -70,6 +70,36 @@ const partyController = {
 
     res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso!" });
   },
+
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+        return;
+      }
+      const updatedParty = await PartyModel.findByIdAndUpdate(id, party);
+
+      if (!updatedParty) {
+        res.status(404).json({ msg: "Festa não encontrada!" });
+        return;
+      }
+      res
+        .status(200)
+        .json({ updatedParty, msg: "Festa atualizada com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -12,4 +12,8 @@ router
   .route("/parties/:id")
   .delete((req, res) => partyController.delete(req, res));
 
+router
+  .route("/parties/:id")
+  .put((req, res) => partyController.update(req, res));
+
 module.exports = router;
